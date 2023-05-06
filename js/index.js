2987924cf933a5970250da3d9a6c9dd2b8815c7b
nicolaprinsloo <231021@virtualwindow.co.za>
2023-05-06 17:01:33 +0200

    exercises attemted

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//Exercise One
+var firstName = "Nicola" ;
+var surname = "Prinsloo" ;
+var integer = "19" ;
+
+const subject = "Development" ;
+
+let message = "Hello my name is " + firstName + " " + surname + ", I am " + integer + " years old and I am studying " + subject + "." ;
+
+console.log(message) ;
+
+//Exercise 2
+let numOne =
+let numTwo =
+let numThree =
+let numFour =
+let answer = 0
+
+let output = numOne + numTwo + numThree + numFour
+
+//Exercise 3
+let timmy = (15/100) * 10 ;
+let susan = (26/100) * 25 ;
+
+let value = timmy + susan ;
+
+let output = "The tax man will claim R" + value + " from Timmy and Susan." ;
+
+console.log(output) ;
+
+//Exercise 4
+let john = 80 ;
+let tim = 70 ;
+let alfie = 60 ;
+let gwen = 50 ;
+
+john = john + 7
+tim = tim + 7
+alfie = alfie + 7
+gwen = gwen + 7
+
+let sum = john + tim +alfie + gwen ;
+let average = (sum/400) * 100 ;
+
+console.log(average) ;
